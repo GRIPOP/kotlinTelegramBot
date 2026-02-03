3b2d82eae7d56ac5f64ce751745ad565a0dc8d61
gpopov <pgriga@mail.ru>
2026-02-03 18:49:11 +0500

    KTB-04-deleted block try\catch, deleted empty body and added checking parts.size

--- app/src/main/kotlin/KTB-02.kt
+++ app/src/main/kotlin/KTB-02.kt
@@ -5,19 +5,14 @@ import java.io.File
 fun main() {
     val dictionary: MutableList<Word> = mutableListOf()
     val wordsFile = File("words.txt")
-
-    try {
-        wordsFile.readLines()
-
-    } catch (e: java.io.FileNotFoundException) {
-        println("Файл отсутствует. Необходимо создать файл со словами.")
-    }
-
     val lines: List<String> = wordsFile.readLines()
+
     for (line in lines) {
-        val line = line.split("|")
-        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
-        dictionary.add(word)
+        val parts = line.split("|")
+        if (parts.size >= 3) {
+            val word = Word(original = parts[0], translate = parts[1], correctAnswersCount = parts[2].toIntOrNull() ?: 0)
+            dictionary.add(word)
+        }
     }
 
     println(dictionary)
--- app/src/main/kotlin/Word.kt
+++ app/src/main/kotlin/Word.kt
@@ -3,7 +3,5 @@ package org.example.app
 data class Word(
     val original: String,
     val translate: String,
-    var correctAnswersCount: Int? = 0,
-) {
-
-}
+    var correctAnswersCount: Int = 0,
+)
