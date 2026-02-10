523c37dffa50f5d82e660943a2031bd3bc284d1f
gpopov <pgriga@mail.ru>
2026-02-10 18:39:13 +0500

    KTB-12-http requests getMe and getUpdates

---
+++ app/src/main/kotlin/Telegram.kt
@@ -0,0 +1,22 @@
+package org.example.app
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main(args: Array<String>) {
+    val botToken = args[0]
+    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
+
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val requestGetMe: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
+    val responseGetMe: HttpResponse<String> = client.send(requestGetMe, HttpResponse.BodyHandlers.ofString())
+    println(responseGetMe.body())
+
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+
+    val requestGetUpdates: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val responseGetUpdates: HttpResponse<String> = client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString())
+    println(responseGetUpdates.body())
+}
