ce4e3284cc5135de6e086bf27623c8ba7aa98f21
gpopov <pgriga@mail.ru>
2026-03-06 22:00:53 +0500

    KTB-23-shadow V2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     id("buildsrc.convention.kotlin-jvm")
     kotlin("plugin.serialization") version "2.3.10"
-    id("com.github.johnrengelman.shadow") version "8.3.0"
+    id("com.gradleup.shadow") version "8.3.0"
 
     // Apply the Application plugin to add support for building an executable JVM application.
     application
@@ -19,5 +19,5 @@ dependencies {
 application {
     // Define the Fully Qualified Name for the application main class
     // (Note that Kotlin compiles `App.kt` to a class with FQN `com.example.app.AppKt`.)
-    mainClass = "org.example.app.AppKt"
+    mainClass.set("org.example.app.TelegramKt")
 }
